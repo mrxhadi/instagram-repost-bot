80c3383f43e5741acb86045dcbc80ce09a8a74a0
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-02-07 23:39:39 +0330

    Update downloader.py

--- bot/downloader.py
+++ bot/downloader.py
@@ -9,10 +9,12 @@ video_urls = [
 ]
 
 def download_instagram_reel(video_url):
-    ydl_opts = {
-        'outtmpl': f"{DOWNLOAD_FOLDER}/%(id)s.%(ext)s",
-        'format': 'bestvideo+bestaudio/best',
-    }
+   ydl_opts = {
+    'outtmpl': "downloads/%(id)s.%(ext)s",
+    'format': 'bestvideo+bestaudio/best',
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(video_url, download=True)
         return info['id'], info['ext']
